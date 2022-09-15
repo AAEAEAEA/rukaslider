98b213f83ebe95804bdaca5ac0441d1cf2b16ca0
AAEAEAEA <chkhaidzenika27@gmail.com>
2022-09-15 17:29:03 +0400

    rukaslider

---
+++ Js/scripts.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    freeDrag:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
